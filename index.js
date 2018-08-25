0aed493d61dbd6b770de03ff266d135dc10be5c4
Samantha Hyatt <shhyatt@gmail.com>
2018-08-25 02:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,9 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
+
 
