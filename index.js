c4da7e7d2cae221adc5939fa3fa63185b43c693a
Samantha Hyatt <shhyatt@gmail.com>
2018-08-25 01:57:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return "`Happy holidays, ${name}!`"
 }
 function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+  return "`Happy ${holiday}, ${name}!`"
 }
 
