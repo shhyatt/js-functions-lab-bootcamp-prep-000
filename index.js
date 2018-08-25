688499182bef366cb1ebb8800465cb54b705483c
Samantha Hyatt <shhyatt@gmail.com>
2018-08-25 01:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
+  console.log(`Happy holidays, Sam!`)
 }
-happyHolidaysTo('Sam')
+
